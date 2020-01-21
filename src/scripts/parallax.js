e9f6ed2096500cdc22c914e2d6fa53ba6cc1e4dc
DDanielov <dandelmin@gmail.com>
2020-01-22 02:04:23 +0300

    работа над параллаксом для верхней части ленда

---
+++ src/scripts/parallax.js
@@ -0,0 +1,42 @@
+console.log('hello parallax!');
+
+parallax = (function () {
+    let layer1 = document.querySelector('.parallax__layer_sky'),
+        layer2 = document.querySelector('.parallax__layer_mountain'),
+        layer3 = document.querySelector('.parallax__layer_baloon'),
+        layer4 = document.querySelector('.parallax__layer_cloud'),
+        layer5 = document.querySelector('.parallax__layer_man'),
+        layer6 = document.querySelector('.parallax__layer_mountain1'),
+        layer7 = document.querySelector('.parallax__layer_white');
+
+    return {
+        move: function (layer, windowScroll, strafeAmount) {
+            let strafe = windowScroll / -strafeAmount + '%',
+                transfotmString = 'translate3d(0,' + strafe + ', 0)',
+                transformBaloonString = 'translate3d(' + strafe + ',' + strafe + ', 0)',
+                style = layer.style;
+
+            if( layer === layer3) {
+                style.transform = transformBaloonString;
+                style.webkitTransform = transformBaloonString;
+            } else {
+                style.transform = transfotmString;
+                style.webkitTransform = transfotmString;
+            }
+        },
+        init: function (wScroll) {
+            this.move(layer1, wScroll, 70);
+            this.move(layer2, wScroll, 50);
+            this.move(layer3, wScroll, 10);
+            this.move(layer4, wScroll, 40);
+            this.move(layer5, wScroll, 80);
+            this.move(layer6, wScroll, 95);
+            this.move(layer7, wScroll, 100);
+        }
+    }
+}());
+
+window.onscroll = function () {
+    let wScroll = window.pageYOffset;
+    parallax.init(wScroll);
+};
